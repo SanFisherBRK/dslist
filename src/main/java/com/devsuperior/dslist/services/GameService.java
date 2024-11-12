5e07584fadd2957ab4067e3e268a407ed0a5dbe8
Souza <sanfisher@msn.com>
2024-11-12 16:05:40 -0300

    Buscar lista de games por id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,6 +4,7 @@ import com.devsuperior.dslist.Repository.GameRepository;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projectios.GameMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,6 +29,13 @@ public class GameService {
        List<Game> result =  gameRepository.findAll();
        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList(); // Convertendo List Game em List GameMinDTO
        return dto;
+    }
+
+    @Transactional(readOnly = true) //Mostra que é somente para leitura e não escrita
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result =  gameRepository.searchByList(listId);
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList(); // Convertendo List Game em List GameMinDTO
+        return dto;
     }
 
 
