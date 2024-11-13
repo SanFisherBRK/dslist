478c4844f1ed7929f5e81a1c3d57909d01fba51f
Souza <sanfisher@msn.com>
2024-11-13 19:41:17 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dslist/projectios/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/projectios/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.devsuperior.dslist.projectios;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
